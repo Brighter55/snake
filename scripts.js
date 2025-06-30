8f163a113495ea373fe4cfd38bffbbb54cb31ebd
Piyawit Sriphrakhun <sriphrakhunpiyawit@gmail.com>
2025-06-29 22:10:58 -0500

    finish snake body

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
+function create_box() {
+    ctx.moveTo(0, 0);
+    ctx.lineTo(300, 0);
+    ctx.lineTo(300, 300);
+    ctx.lineTo(0, 300);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+}
+
+function draw_snake_body(coordinates) { // call function to draw each body block for each coordinates
+    ctx.fillStyle = "lightgreen";
+    ctx.strokeStyle = "darkgreen";
+    ctx.fillRect(coordinates.x, coordinates.y, 10, 10);
+    ctx.strokeRect(coordinates.x, coordinates.y, 10, 10);
+}
+
+// call function that make block for body for every block.
+function draw_snake() {
+    for (let i = 0; i < snake.length; i++) {
+        draw_snake_body(snake[i]);
+    }
+}
+
 var container = document.getElementById("container");
 var ctx = container.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(300, 0);
-ctx.lineTo(300, 300);
-ctx.lineTo(0, 300);
-ctx.lineTo(0, 0);
 
-ctx.stroke();
+create_box();
+var snake = [{x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150}
+];
+draw_snake()
